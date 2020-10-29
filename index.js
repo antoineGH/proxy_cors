942589f0608e69ab42b3dca8234ef3692688887a
Templars <antoine.ratat@gmail.com>
2020-10-29 15:42:41 +0800

    80

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ app.get('/cityfalcon/:identifiers/:language/:token', (req, res) => {
 	)
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 80
 app.listen(PORT, () => console.log(`listening on ${PORT}`))
