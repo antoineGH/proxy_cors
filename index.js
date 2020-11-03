dd4955a17fc28c4bda6c3df84babde9092f99eb6
Templars <antoine.ratat@gmail.com>
2020-11-03 23:08:06 +0800

    Add Tickers as Identifier (new route)

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
 	next()
 })
 
-// City Falcon Endpoint
+// City Falcon Endpoint Identifier
 app.get('/cityfalcon/:identifiers/:language/:token', (req, res) => {
 	const identifiers = req.params.identifiers
 	const language = req.params.language
@@ -16,6 +16,25 @@ app.get('/cityfalcon/:identifiers/:language/:token', (req, res) => {
 	request(
 		{
 			url: `https://sandbox-api.cityfalcon.com/v0.2/stories?identifier_type=assets&identifiers=${identifiers}&time_filter=mth1&categories=mp%2Cop&min_cityfalcon_score=0&order_by=top&languages=${language}&all_languages=false&access_token=${token}`,
+		},
+		(error, response, body) => {
+			if (error || response.statusCode !== 200) {
+				return res.status(500).json({ type: 'error', message: err.message })
+			}
+
+			res.json(JSON.parse(body))
+		}
+	)
+})
+
+// City Falcon Endpoint
+app.get('/tickers/:tickers/:language/:token', (req, res) => {
+	const tickers = req.params.tickers
+	const language = req.params.language
+	const token = req.params.token
+	request(
+		{
+			url: `https://sandbox-api.cityfalcon.com/v0.2/stories?identifier_type=assets&identifiers=${tickers}&time_filter=mth1&categories=mp%2Cop&min_cityfalcon_score=0&order_by=top&languages=${language}&all_languages=false&access_token=${token}`,
 		},
 		(error, response, body) => {
 			if (error || response.statusCode !== 200) {
