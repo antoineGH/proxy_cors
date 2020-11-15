c80c414513f3f306c57fe1e0ddcaebfaa6595ffa
Templars <antoine.ratat@gmail.com>
2020-11-15 12:20:15 +0800

    update url endpoint API test

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/cityfalcon/:identifiers/:language/:token', (req, res) => {
 	request(
 		{
 			// url: `https://sandbox-api.cityfalcon.com/v0.2/stories?identifier_type=assets&identifiers=${identifiers}&time_filter=mth1&categories=mp%2Cop&order_by=top&languages=${language}&all_languages=false&access_token=${token}`,
-			url: `https://sandbox-api.cityfalcon.com/v0.2/stories?identifier_type=assets&identifiers=Microsoft&access_token=9e522ad481d49a67ba237d3445b5eea849576a83e8ab46f9911f30406c42f810`,
+			url: `https://sandbox-api.cityfalcon.com/v0.2/stories?identifier_type=assets&identifiers=Microsoft, Apple&time_filter=mth1&access_token=9e522ad481d49a67ba237d3445b5eea849576a83e8ab46f9911f30406c42f810`,
 		},
 		(error, response, body) => {
 			if (error || response.statusCode !== 200) {
